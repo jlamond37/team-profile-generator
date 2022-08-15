d77cffab6fdb8ed6ff1048f104d86687dc1f7c54
jlamond37 <jlamond37@gmail.com>
2022-08-15 17:24:47 -0400

    tests

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,5 +1,5 @@
 // using Employee constructor 
-const Employee = require('../lib/Employee');
+// const Employee = require('../lib/Employee');
 
 // creates employee object 
 test('creates an employee object', () => {
